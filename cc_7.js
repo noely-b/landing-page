8c046395a1adda0238df2ec198f5838c5c2e3b84
Noely <noely.bosque@gmail.com>
2025-07-05 16:37:50 -0400

    final

--- cc_7.js
+++ cc_7.js
@@ -1,1 +1,6 @@
-
+document.getElementById('CTA-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const newHeadline = document.getElementById('new-headline').value;
+    document.getElementById('CTA-headline').textContent = newHeadline;
+    this.reset();
+});
